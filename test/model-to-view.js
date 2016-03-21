01dc06054f45f2b51284450e021aadb9536df9b2
Sean Gilberston <sean.gilbertson@buzzfeed.com>
2016-03-21 09:18:56 -0500

    Add testing dependencies

--- test/model-to-view.js
+++ test/model-to-view.js
@@ -1,6 +1,40 @@
+/**
+ * @extends {jQuery}
+ * @type {jQuery}
+ */
+var $ = window.Sprint;
+
+/**
+ * @class
+ * @extends {Backbone.Model}
+ */
+var MyModel = Backbone.Model.extend({});
+
+/**
+ * @class
+ * @extends {Backbone.View}
+ */
+var MyView = Backbone.View.extend({
+  /**
+   * @constructor
+   */
+  initialize: function() {
+    if (Backbone.View.prototype.initialize) {  // call super method
+      Backbone.View.prototype.initialize.apply(this, arguments);
+    }
+    console.log('- here');
+    console.log(this.el);
+  }
+});
+
+
 QUnit.test(
   'model-view binding tests',
   function (assert) {
-    assert.ok(true, 'hello there');
+    var viewEl = document.createElement('div');
+    viewEl.classList.add('js-test-el');
+    var view = new MyView({ el: viewEl });
+    assert.ok(view);
+    assert.ok(view.el);
   }
 );
